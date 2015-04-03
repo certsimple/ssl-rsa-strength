e1dd2efe1b67b1c514175b9ae440573310cc4aeb
Mike MacCana <mike.maccana@gmail.com>
2015-04-03 12:26:21 +0100

    Note re: Unit tests

--- test/tests.js
+++ test/tests.js
@@ -8,7 +8,9 @@ var getRSAStrength = require('../index.js')
 var log = console.log.bind(console)
 
 suite('RSA strength', function(){
-	// Values checked against Mathematica implementation
+	// Values checked against Reid Wiggin's Mathematica implementation
+	// ({#, N@Log2@g[#]} &) /@ {1024, 2048, 4096, 8192}
+	// g[b_] := Exp[(64/9 * Log[2^b])^(1/3) * (Log[Log[2^b]])^(2/3)]
 	test('is calculated correctly', function(){
 		assert.equal(getRSAStrength(512), 63)
 		assert.equal(getRSAStrength(1024), 86)
